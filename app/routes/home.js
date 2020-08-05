5575406b73f2adb6c3946b52296e405faef2cc35
Juliano Costa Silva <apfjuliano@gmail.com>
2020-08-05 13:01:37 -0300

    First commit

---
+++ app/routes/home.js
@@ -0,0 +1,7 @@
+// app/routes/home.js 
+var controller = require('../controllers/home')();
+
+module.exports = function(app) {
+    app.get('/index', controller.index);
+    app.get('/', controller.index);
+}
