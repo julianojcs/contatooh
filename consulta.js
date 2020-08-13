0ae2a5800c44a8a9258aa3e5338a6aacc19b7ea8
Juliano Costa Silva <apfjuliano@gmail.com>
2020-08-13 00:30:40 -0300

    Integrating Express with MongoDB

--- consulta.js
+++ consulta.js
@@ -1,16 +1,16 @@
 // contatooh/consulta.js 
 var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://127.0.0.1:27017/contatooh";
 var ObjectID = require('mongodb').ObjectID;
 
 // ObjectID de algum contato existente 
-var _idProcurado = new ObjectID('53ee689e89bd201218944bba');
+var _idProcurado = new ObjectID('5f34b063ca897c78c67c7e15');
 
-MongoClient.connect('mongodb://127.0.0.1:27017/contatooh', {useUnifiedTopology: true, useNewUrlParser: true }, function (erro, db) {
-    if (erro) 
-        throw err;
-    db.collection('contatos').findOne({_id: _idProcurado}, function (erro, contato) {
-        if (erro) 
-            throw err;
+MongoClient.connect(url, {useUnifiedTopology: true, useNewUrlParser: true }, function (erro, client) {
+    if (erro) throw err;
+    const db = client.db("contatooh");
+    db.collection('contatooh').findOne({"_id": _idProcurado}, function (erro, contato) {
+        if (erro) throw err;
         console.log(contato);
     });
 });
