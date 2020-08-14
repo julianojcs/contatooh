457231ba0bcc8f3152989e466ea76bb240160aac
Juliano Costa Silva <apfjuliano@gmail.com>
2020-08-14 01:02:09 -0300

    MongoDB Dependencies

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -1,60 +1,28 @@
 // app/controllers/contato.js 
 
-var contatos = [ 
-    {_id: 1, nome: 'Contato Exemplo 1' , email: 'cont1@empresa.com.br' }, 
-    {_id: 2, nome: 'Contato Exemplo 2' , email: 'cont2@empresa.com.br' }, 
-    {_id: 3, nome: 'Contato Exemplo 3' , email: 'cont3@empresa.com.br' }
-];
-
-var ID_CONTATO_INC = contatos.length;
-
-module.exports = function() {
+module.exports = function(app) {
+    var Contato = app.models.contato;
     var controller = {};
 
-    controller.listaTodos = function(req, res) { 
-        // envia a lista
-        res.json(contatos);
-    };
-
-    controller.salvaContato = function(req, res) {
-        var contato = req.body; // req.body -> Variáveis do formulário usando o método POST. (equivale ao $_POST do PHP)
-                                // req.query -> Variáveis da URL usando método GET(equivale ao $_GET do PHP)
-                                // req.params -> Variáveis das rotas da URL (ex: localhost/contatooh/contato/1) -> param = 1 
-        contato = contato._id ? atualiza(contato) : adiciona(contato);
-        res.json(contato); 
+    controller.listaTodos = function(req, res) {
+        var promise = Contato.find().exec()
+            .then( function(contatos) {
+                res.json(contatos);
+            },
+            function(erro) {
+                console.error(erro);
+                res.status(500).json(erro);
+            }
+        );
     };
     
-    function adiciona(contatoNovo) {
-        contatoNovo._id = ++ID_CONTATO_INC;
-        contatos.push(contatoNovo);
-        return contatoNovo;
-    }
-    function atualiza(contatoAlterar) {
-        contatos = contatos.map(function(contato) {
-            if(contato. _id == contatoAlterar._id) {
-                contato = contatoAlterar;
-            }
-            return contato;
-        }); 
-        return contatoAlterar;
-    }
-
     controller.obtemContato = function(req, res) {
-        console.log(req.params.id); 
-        var idContato = req.params.id; 
-        var contato = contatos.filter(function(contato) { 
-            return contato._id == idContato;
-        })[0];
-        return contato ? res.json(contato) : res.status(404).send('Contato não encontrado');
     };
 
     controller.removeContato = function(req, res) {
-        var idContato = req.params.id; 
-        console.log('API: removeContato: ' + idContato);
-        contatos = contatos.filter(function(contato) { 
-            return contato._id != idContato;
-        });
-        res.sendStatus(204).end();
+    };
+
+    controller.salvaContato = function(req, res) {
     };
 
     return controller;
--- consulta.js
+++ consulta.js
@@ -1,12 +1,12 @@
 // contatooh/consulta.js 
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://127.0.0.1:27017/contatooh";
 var ObjectID = require('mongodb').ObjectID;
 
 // ObjectID de algum contato existente 
 var _idProcurado = new ObjectID('5f34b063ca897c78c67c7e15');
+var url = "mongodb://127.0.0.1:27017/contatooh";
 
-MongoClient.connect(url, {useUnifiedTopology: true, useNewUrlParser: true }, function (erro, client) {
+MongoClient.connect(url, {useUnifiedTopology: true, useNewUrlParser: true}, function (erro, client) {
     if (erro) throw err;
     const db = client.db("contatooh");
     db.collection('contatooh').findOne({"_id": _idProcurado}, function (erro, contato) {
