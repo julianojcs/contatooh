35129d9777d6adcc1ce6aef268c82afbb4c90500
Juliano Costa Silva <apfjuliano@gmail.com>
2020-08-16 22:52:44 -0300

    Mongoose DB Config

---
+++ app/models/contato.js
@@ -0,0 +1,20 @@
+// app/models/contato.js
+
+var mongoose = require('mongoose');
+
+module.exports = function() {
+    var schema = mongoose.Schema({
+        nome: { 
+            type: String,
+            required: true
+        },
+        email: { 
+            type: String,
+            required: true,
+            index: {
+                unique: true
+            }
+        }
+    });
+    return mongoose.model('Contato' , schema);
+};
